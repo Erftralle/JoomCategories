dea08c1fc361788f1eae2c7a511c8df3a46bbac6
Erftralle <Erftralle@users.noreply.github.com>
2020-07-09 11:09:48 +0200

    Removes obsolete repository SVN tags

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,6 +1,4 @@
 <?php
-// $HeadURL: https://joomgallery.org/svn/joomgallery/JG-3/Modules/JoomCategories/trunk/tmpl/default.php $
-// $Id: default.php 4314 2013-07-20 09:53:08Z erftralle $
 /**
 * Module JoomCategories for JoomGallery
 * by JoomGallery::Project Team
